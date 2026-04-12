ff1f6e08a29245ea80aa7a979baf2c94cb302bdf
Thammachart <1731496+Thammachart@users.noreply.github.com>
2026-04-12 12:10:54 +0700

    base-main as base, greetd + sway, hyprland

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,15 +10,22 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y \
+    greetd gtkgreet sway
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
+# Use a COPR
+dnf5 -y copr enable lionheartp/Hyprland
+
+dnf5 -y install hyprland hypridle
+
 # Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable lionheartp/Hyprland
+
+dnf5 clean all
+
+cp -v /ctx/greetd/* /etc/greetd/
+
+rm -rf /run/dnf /run/selinux-policy
 
 #### Example for enabling a System Unit File
-
 systemctl enable podman.socket
