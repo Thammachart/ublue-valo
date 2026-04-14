2c2ec815f907933aadb93ea0c82df3df13aa4d97
Thammachart <1731496+Thammachart@users.noreply.github.com>
2026-04-15 03:06:12 +0700

    fuse module loaded early

--- build_files/02-configure/script.sh
+++ build_files/02-configure/script.sh
@@ -2,6 +2,8 @@
 
 set -ouex pipefail
 
+echo "fuse" > /usr/lib/modules-load.d/fuse.conf
+
 cp -v /ctx/kargs.d/* /usr/lib/bootc/kargs.d/
 cp -v /ctx/greetd/config/* /etc/greetd/
 cp -v /ctx/greetd/sessions/* /usr/bin/
