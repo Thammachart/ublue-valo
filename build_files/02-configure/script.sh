8a7b33002d3dd8b9e885131e7ddb776acb9694b8
Thammachart <1731496+Thammachart@users.noreply.github.com>
2026-04-15 15:08:39 +0700

    dracut hostonly + lz4 compression, masked unnessary systemd services

--- build_files/02-configure/script.sh
+++ build_files/02-configure/script.sh
@@ -4,9 +4,13 @@ set -ouex pipefail
 
 echo "fuse" > /usr/lib/modules-load.d/fuse.conf
 
+cp -v /ctx/dracut/* /usr/lib/dracut/dracut.conf.d/
 cp -v /ctx/kargs.d/* /usr/lib/bootc/kargs.d/
 cp -v /ctx/greetd/config/* /etc/greetd/
 cp -v /ctx/greetd/sessions/* /usr/bin/
 
 systemctl enable greetd
 systemctl enable podman.socket
+
+systemctl mask systemd-remount-fs.service
+systemctl mask fedora-atomic-desktop-appstream-cache-refresh.service
