cc2978a6c79d9364424b1c72359dbdad985689a9
Thammachart <1731496+Thammachart@users.noreply.github.com>
2026-05-06 02:36:02 +0700

    fix /opt workaround

--- build_files/00-source/script.sh
+++ build_files/00-source/script.sh
@@ -5,14 +5,15 @@ set -ouex pipefail
 sed -i 's|^\(metalink=.*\)|\1\&country=au,us,jp\&protocol=https|' \
         /etc/yum.repos.d/*.repo
 
-
 cp -v /ctx/repos/* /etc/yum.repos.d/
 
-# Brave official repo and workaround of /opt
+# Brave official repo
 dnf -y config-manager addrepo \
     --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-mkdir -p "/var/opt"
-ln -s "/var/opt" "/opt"
+
+# Workaround of /opt being not persistent (e.g brave)
+rm /opt
+mkdir /opt
 
 # dnf -y copr enable lionheartp/Hyprland
 dnf -y copr enable yalter/niri
